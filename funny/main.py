03812c2e7c29af9d8f542bb1c4aacddd8c37309a
henan.wang <henan.wang@henan.wang's MacBook Pro>
2018-12-14 20:05:49 +0800

    remove head

--- funny/main.py
+++ funny/main.py
@@ -59,14 +59,11 @@ def _load_data():
 
 
 def _load_fm_data():
-    header = ['target', 'id']
-    for i in range(200):
-        header.append('f' + str(i))
-
     dfTrain, y_train, qid_train = load_svmlight_file(config.TRAIN_FILE, query_id=True)
+    print('train x y qid:', dfTrain.shape, y_train.shape, qid_train.shape)
+
     dfTest, y_test, ids_test = load_svmlight_file(config.TEST_FILE, query_id=True)
     dfTest.resize((dfTest.shape[0], 899))
-    print('train x y qid:', dfTrain.shape, y_train.shape, qid_train.shape)
     print('test x y qid:', dfTest.shape, y_test.shape, ids_test.shape)
 
     X_train = pd.DataFrame(dfTrain.toarray())
@@ -151,12 +148,12 @@ def _plot_fig(train_results, valid_results, model_name):
 # load data
 # dfTrain, dfTest, X_train, y_train, X_test, ids_test, cat_features_indices = _load_data()
 dfTrain, dfTest, X_train, y_train, X_test, ids_test, cat_features_indices = _load_fm_data()
-print('dfTrain:', dfTrain.shape, dfTrain.head())
-print('dfTest:', dfTest.shape, dfTest.head())
-print('X_train:', X_train.shape, X_train)
-print('y_train:', y_train.shape, y_train)
-print('X_test:', X_test.shape, X_test)
-print('ids_test:', len(ids_test), ids_test)
+print('dfTrain:', dfTrain.shape)
+print('dfTest:', dfTest.shape)
+print('X_train:', X_train.shape)
+print('y_train:', y_train.shape)
+print('X_test:', X_test.shape)
+print('ids_test:', len(ids_test))
 print('cat_features_indices:', len(cat_features_indices), cat_features_indices)
 
 # folds
