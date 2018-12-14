87479dcf35b334ae74aa9972d1a707469094f457
henan.wang <henan.wang@henan.wang's MacBook Pro>
2018-12-14 13:40:39 +0800

    add funny

---
+++ funny/config.py
@@ -0,0 +1,23 @@
+
+# set the path-to-files
+TRAIN_FILE = "../../dataset/log_preprocess/20181128/train_ph.libsvm"
+TEST_FILE = "../../dataset/log_preprocess/20181128/test_ph.libsvm"
+
+SUB_DIR = "./output"
+
+
+NUM_SPLITS = 3
+RANDOM_SEED = 2018
+
+# types of columns of the dataset dataframe
+CATEGORICAL_COLS = [
+
+]
+
+NUMERIC_COLS = [
+
+]
+
+IGNORE_COLS = [
+
+]
